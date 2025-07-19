9d80756a5a028a41c11a34e1c7ed15c420ed3651
gennaro <g.rascato@gmail.com>
2025-07-19 15:03:48 +0200

    Updates resource locations to West Europe
    Updates the location for the storage, postgres, and function app modules to West Europe.
    
    This ensures all resources are deployed in a single region.

--- main.tf
+++ main.tf
@@ -6,13 +6,13 @@ module "storage_media" {
   source   = "./modules/storage"
   name     = "stmediaadoggamiprod"
   rg_name  = azurerm_resource_group.project.name
-  location = "francecentral"  # Explicit location
+  location = "westeurope"  # Explicit location
 }
 module "postgres_dev" {
   source     = "./modules/postgres"
   name       = "pg-adoggami-prod"
   rg_name    = azurerm_resource_group.project.name
-  location   = "francecentral"  # Explicit location
+  location   = "westeurope"  # Explicit location
   admin_user = var.pg_admin
   admin_pass = var.pg_pass
 }
@@ -20,7 +20,7 @@ module "func_petcatalog" {
   source        = "./modules/pet_catalog_service"
   name          = "func-petcatalog-prod"
   rg_name       = azurerm_resource_group.project.name
-  location      = "francecentral"  # Explicit location
+  location      = "westeurope"  # Explicit location
   sa_name       = module.storage_media.name
   sa_key        = module.storage_media.primary_access_key
   postgres_conn = module.postgres_dev.connection_string
