398e9a0690e573b8ccba800775d9c259ddc20062
gennaro <g.rascato@gmail.com>
2025-07-22 03:02:05 +0200

    fix

--- modules/postgres/main.tf
+++ modules/postgres/main.tf
@@ -7,6 +7,6 @@ resource "azurerm_postgresql_flexible_server" "this" {
   administrator_password = var.admin_pass
   storage_mb             = var.storage_mb
   version                = "15"
-  
+  zone                   = "2"
   tags                   = var.tags
 }
